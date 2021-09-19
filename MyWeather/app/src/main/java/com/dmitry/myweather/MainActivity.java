794297a37d4abff669252957893e4c5c20d0a367
”Junior” <rip05-04-1990@yandex.ru>
2021-09-20 01:39:37 +0300

    a little new

--- MyWeather/app/src/main/java/com/dmitry/myweather/MainActivity.java
+++ MyWeather/app/src/main/java/com/dmitry/myweather/MainActivity.java
@@ -3,12 +3,41 @@ package com.dmitry.myweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    TextView city;
+    TextView temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        city = findViewById(R.id.city);
+        temperature = findViewById(R.id.temperature);
+
+        city.setText("Нижний Новгород");
+        JSONObject object = GetWeather.getJSON(this, "520555");
+        String temp = "0";
+
+        //temp = object.has("main") + "";
+
+//        try {
+//            JSONObject array = object.getJSONObject("main");
+//            temp = array.getDouble(0) + " C";
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            temp = object.getDouble("temp") + " C";
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+        temperature.setText(temp);
     }
 }
