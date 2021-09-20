e28eb8bae844c552ff2c1cb6adb85cf17028eee2
”Junior” <rip05-04-1990@yandex.ru>
2021-09-20 22:56:50 +0300

    starting Alishev Android course, lesson 3

---
+++ AlishevAndroidCourse/ListApp/app/src/main/java/com/dmitry/listapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.dmitry.listapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView plList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_list);
+
+        String[] names = {"Java", "Python", "JavaScript", "C++"};
+
+        plList.setText("");
+        for(String s: names) {
+            plList.append(s + "\n");
+        }
+    }
+}
