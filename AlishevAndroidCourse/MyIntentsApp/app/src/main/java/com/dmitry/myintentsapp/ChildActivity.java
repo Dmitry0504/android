1b99e250795c4ffa5c7f0049745a69c83ca8044c
”Junior” <rip05-04-1990@yandex.ru>
2021-09-28 23:02:06 +0300

    lesson 17 implicit intents

--- AlishevAndroidCourse/MyIntentsApp/app/src/main/java/com/dmitry/myintentsapp/ChildActivity.java
+++ AlishevAndroidCourse/MyIntentsApp/app/src/main/java/com/dmitry/myintentsapp/ChildActivity.java
@@ -2,26 +2,71 @@ package com.dmitry.myintentsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
-    TextView textView;
+    EditText et_input;
+    Button btn_send;
+    Button btn_browser;
+    Button btn_map;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+        context = ChildActivity.this;
 
-        textView = findViewById(R.id.display);
 
-        Intent intent = getIntent();
+        et_input = findViewById(R.id.et_input);
+        btn_send = findViewById(R.id.btn_send_msg);
+        btn_browser = findViewById(R.id.btn_search_in_browser);
+        btn_map = findViewById(R.id.btn_open_map);
 
-        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String message = intent.getStringExtra(Intent.EXTRA_TEXT);
 
-            textView.setText(message);
-        }
+
+
     }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        String request = et_input.getText().toString();
+
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, request);
+
+        startActivity(intent);
+    }
+
+    public void searchInBrowser(View view) {
+        String request = String.format("https://www.google.com/search?q=%s", et_input.getText().toString());
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(request));
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+    public void openMap(View view) {
+        String address = et_input.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = Uri.parse("geo:0,0").buildUpon()
+                .appendQueryParameter("q", address).build();
+        intent.setData(uri);
+
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+        else {
+            intent.setData(Uri.parse("geo:56.315341,44.061628"));
+            startActivity(intent);
+        }
+
+    }
+
 }
